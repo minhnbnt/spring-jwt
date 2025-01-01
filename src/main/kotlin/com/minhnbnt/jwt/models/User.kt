1d8234835fc29348461d0045ba32ab4c16e5dccb
minhnbnt <minhnbnt@gmail.com>
2025-01-01 13:26:33 +0700

    feat: define index on username

--- src/main/kotlin/com/minhnbnt/jwt/models/User.kt
+++ src/main/kotlin/com/minhnbnt/jwt/models/User.kt
@@ -5,17 +5,26 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
+@Table(
+    indexes = [
+        Index(
+            name = "idx_username",
+            columnList = "username",
+            unique = true,
+        )
+    ]
+)
 class User(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0,
+    val id: Long = 0,
 
     @Column(nullable = false, unique = true)
     private var username: String,
 
     @Column(nullable = false)
-    private var password: String
+    private var password: String,
 
 ) : UserDetails {
 
